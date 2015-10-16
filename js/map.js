2c68ae03116ccf96fe898c6b41e448753fd4dc2a
Joel Elizaga <joelizaga@hotmail.com>
2015-10-16 15:08:53 -0700

    Switched map, commented out old one just in case.

--- js/map.js
+++ js/map.js
@@ -1,8 +1,20 @@
 // drawMap draws the map, then calls getData.
 var drawMap = function() {
 	alert("drawMap executed.");
+	/* The mapbox plugin makes this a bit messy. I included my accessToken and set the tileLayer to a 
+	custom map I made in their "Mapbox Studio" application. */
+	L.mapbox.accessToken = "pk.eyJ1IjoiamVsaXphZ2EiLCJhIjoiY2lmdTJvMzMxMWl2MHRnbHlzOXUzdjE1biJ9.IT8SuuUQ8B9OwJWOIu4qhQ";
+	var layer = L.tileLayer("http://{s}.tiles.mapbox.com/v4/jelizaga.c693687d/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamVsaXphZ2EiLCJhIjoiY2lmdTJvMzMxMWl2MHRnbHlzOXUzdjE1biJ9.IT8SuuUQ8B9OwJWOIu4qhQ");
+	var map = L.map("container", {
+		center: [40, -90],
+		zoom: 4
+	});
+	/* 
+	OSM map:
 	var map = L.map("container").setView([30, -100], 4);
 	var layer = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png");
+	layer.addTo(map);
+	*/
 	layer.addTo(map);
 	getData();
 }
